2c48201f50e275e71981ea8c268a7570324d98b3
amc <amc>
2025-04-12 16:57:13 +0200

    Working temp

--- budzet-app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class budzet-appConfig(AppConfig):
-    name = 'budzet-app'
---
+++ budzetApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+#from .forms import UserLoginForm, FoodForm
+#from .models import User, Food
+
+
+# Create your views here.
+
+def index(request):
+    return render(request,"budzetApp/index.html")
