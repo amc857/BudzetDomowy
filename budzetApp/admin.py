06caa2cfb289cac1f4a86cc38841c9cb05c90288
amc <amc>
2025-04-14 18:03:17 +0200

    přidání databáze a admin Username: admin Pass: admin

--- budzetApp/admin.py
+++ budzetApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Group)
+admin.site.register(GroupMember)
+admin.site.register(Budget)
+admin.site.register(Transaction)
--- budzetApp/models.py
+++ budzetApp/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    category_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_groups')
+
+    def __str__(self):
+        return self.group_name
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    joined_at = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(max_length=20)  # np. "admin", "member"
+
+    class Meta:
+        unique_together = ('group', 'user')
+
+    def __str__(self):
+        return f"{self.user} in {self.group} as {self.role}"
+
+class Budget(models.Model):
+    budget_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.budget_amount} for {self.category}"
+
+class Transaction(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_date = models.DateField()
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.amount} by {self.user}"
