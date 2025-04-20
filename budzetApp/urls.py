b2a3d7391459f418fa8e1db799541715853a14ef
Jakub Różycki <jr305142@student.polsl.pl>
2025-04-20 23:33:58 +0200

    transakcje

--- budzetApp/urls.py
+++ budzetApp/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+﻿from django.urls import path
+from .views import (
+    TransactionListView,
+    TransactionCreateView,
+    TransactionUpdateView,
+    TransactionDeleteView
+)
 
-from . import views
-
-app_name = "budzetApp"
+app_name = 'budzetApp'
 
 urlpatterns = [
-    path("", views.login, name='login'),
-    path("index/", views.index, name="index"),
-    
+    path('', TransactionListView.as_view(), name='index'),
+    path('index/', TransactionListView.as_view(), name='index'),
+    path('transaction/add/', TransactionCreateView.as_view(), name='add_transaction'),
+    path('transaction/edit/<int:pk>/', TransactionUpdateView.as_view(), name='edit_transaction'),
+    path('transaction/delete/<int:pk>/', TransactionDeleteView.as_view(), name='delete_transaction'),
 ]
--- budzetApp/views.py
+++ budzetApp/views.py
@@ -1,15 +1,67 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+﻿from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Transaction, Category
+from .forms import TransactionForm
+from django.db.models import Sum
 
-#from .forms import UserLoginForm, FoodForm
-#from .models import User, Food
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = "budzetApp/index.html"
+    context_object_name = "transactions"
+    ordering = ['-transaction_date']
+    paginate_by = 10
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_income'] = Transaction.objects.filter(
+            amount__gt=0
+        ).aggregate(Sum('amount'))['amount__sum'] or 0
+        context['total_expenses'] = Transaction.objects.filter(
+            amount__lt=0
+        ).aggregate(Sum('amount'))['amount__sum'] or 0
+        return context
 
-# Create your views here.
+class TransactionCreateView(CreateView):
+    model = Transaction
+    form_class = TransactionForm
+    template_name = "budzetApp/addtransaction.html"
+    success_url = reverse_lazy('budzetApp:index')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['transaction'] = None
+        context['categories'] = Category.objects.all()
+        return context
+
+class TransactionUpdateView(UpdateView):
+    model = Transaction
+    form_class = TransactionForm
+    template_name = "budzetApp/addtransaction.html"
+    success_url = reverse_lazy('budzetApp:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['transaction'] = self.object
+        context['categories'] = Category.objects.all()
+        return context
+
+class TransactionDeleteView(DeleteView):
+    model = Transaction
+    template_name = "budzetApp/confirm_delete.html"
+    success_url = reverse_lazy('budzetApp:index')
+
 
 def index(request):
-    return render(request,"budzetApp/index.html")
-
-
-def login(request):
-    return render(request, 'budzetApp/login.html')
+    transactions = Transaction.objects.all().order_by('-transaction_date')[:10]
+    total_income = Transaction.objects.filter(amount__gt=0).aggregate(Sum('amount'))['amount__sum'] or 0
+    total_expenses = Transaction.objects.filter(amount__lt=0).aggregate(Sum('amount'))['amount__sum'] or 0
+    return render(request, 'budzetApp/index.html', {
+        'transactions': transactions,
+        'total_income': total_income,
+        'total_expenses': total_expenses,
+    })
